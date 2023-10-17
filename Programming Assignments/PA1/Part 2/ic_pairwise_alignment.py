534952d4c9701dcb9de2e142cf68548d02f43ceb
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-10-17 10:39:13 -0700

    part 2 complete

--- Programming Assignments/PA1/Part 2/ic_pairwise_alignment.py
+++ Programming Assignments/PA1/Part 2/ic_pairwise_alignment.py
@@ -19,27 +19,13 @@ AUTHOR: Ian Chavez
       All Rights Reserved
 
 ========================== MODIFICATION HISTORY ==============================
-09/26/23:
-    MOD:     Creation of file and initial organization
-    AUTHOR:  Ian Chavez
-    COMMENT:
-    - Changed package from pairwise2 to Align based on error:
-    "BiopythonDeprecationWarning: Bio.pairwise2 has been deprecated, and we intend to remove it in a future release of Biopython. As an alternative, please consider using Bio.Align.PairwiseAligner as a replacement, and contact the Biopython developers if you still need the Bio.pairwise2 module."
-
-    - Addressed following overflow error by using documentation found at: http://biopython.org/DIST/docs/tutorial/Tutorial.html
-    "OverflowError: number of optimal alignments is larger than 9223372036854775807"
-
-    - Getting ValueError from running
-    "ValueError: sequence contains letters not in the alphabet"
-    
 10/17/23:
-    MOD:     Part 1 done
+    MOD:     Creation of file for part 2
     AUTHOR:  Ian Chavez
     COMMENT:
-    - Changed back to pairwise2 to keep consistent with assignment. 
-    Not trying to lose points because of a warning.
+    - Copy file from Part 1, removed local alignment (unnecessary part 2)
 
-    - Addressed multiple errors and warnings by using documentation found at: http://biopython.org/DIST/docs/tutorial/Tutorial.html
+    - Experimenting with different parameters to improve global score
 
 ====================== END OF MODIFICATION HISTORY ============================
 """
@@ -65,18 +51,9 @@ print("Matrix created")
 # Global alignment using BLOSUM62 matrix
 # and print the results to the console (using format_alignment function)
 print("Global alignment starting====================")
-for a in pairwise2.align.globaldx(seq1, seq2, matrix):
+for a in pairwise2.align.globalmx(seq1, seq2, 6, -1):
     print(pairwise2.format_alignment(*a))
 print("Print global successful====================")
-
-
-# Local alignment using BLOSUM62 matrix
-# and print the results to the console (using format_alignment function)
-print("Local alignment starting====================")
-alignments = pairwise2.align.localdx(seq1, seq2, matrix)
-for a in alignments:
-    print(pairwise2.format_alignment(*a))
-print("Local alignment successful====================")
 
 # Close the files
 seq1_file.close()
